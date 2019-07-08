7cc5dabbf386834fbf830bee6e30aeb235460c19
ismail.ermis <ismail.ermis@demirorenteknoloji.com>
2019-07-08 11:22:18 +0300

    ilk commit

---
+++ Customer.Domain/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Customer.Domain
+{
+    public class Customer : Entity
+    {
+        public string FullName { get; protected set; }
+        public string CityCode { get; protected set; }
+        public DateTime BirthDate { get; protected set; }
+
+        public Customer(string fullName, string cityCode, DateTime birthDate)
+        {
+            if (
+                string.IsNullOrEmpty(fullName) ||
+                string.IsNullOrEmpty(cityCode) ||
+                birthDate.Date == DateTime.Today)
+            {
+                throw new Exception("Geçerli bir müşteri giriniz");
+            }
+
+            FullName = fullName;
+            CityCode = cityCode;
+            BirthDate = birthDate;
+        }
+
+        protected Customer()
+        {
+            
+        }
+
+        public void SetFields(string fullName, string cityCode, DateTime birthDate)
+        {
+            if (string.IsNullOrEmpty(fullName) ||
+                string.IsNullOrEmpty(cityCode) ||
+                birthDate.Date == DateTime.Today)
+            {
+                throw new Exception("Güncelleme için alanlar geçerli değil.");
+            }
+
+            FullName = fullName;
+            CityCode = cityCode;
+            BirthDate = birthDate;
+        }
+    }
+}
---
+++ Customer/Startup.cs
@@ -0,0 +1,55 @@
+﻿using Customer.Domain;
+using Customer.Repository;
+using Customer.Service;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Customer.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ConfigureDatabase(services);
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<ICustomerService, CustomerService>();
+
+            services.AddTransient<ICustomerAssembler, CustomerAssembler>();
+            services.AddTransient<ICustomerService, CustomerService>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseMvc();
+        }
+
+        public virtual void ConfigureDatabase(IServiceCollection services)
+        {
+            services.AddDbContext<CustomerDbContext>(options =>
+                options.UseSqlServer(Configuration.GetSection("CustomerDbConnString").Value));
+        }
+    }
+}
